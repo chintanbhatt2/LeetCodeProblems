c4072568666b2fb974fc3a19a3496ce50eaaef41
Chintan Bhatt <chintanbhatt2@gmail.com>
2021-04-22 23:40:10 -0700

    Changed algo on 332

--- 332. Reconstruct Itinerary.py
+++ 332. Reconstruct Itinerary.py
@@ -1,25 +1,51 @@
-class Flight:
-    def __init__(self, tickets: list):
-        self.start = tickets[0]
-        self.end = tickets[1]
-        self.redeemed = False
+class Airport:
+    def __init__(self, name: str):
+        self.code = name
+        self.outbound =[]
+    def addOutbound(self, string):
+        self.outbound.append(string)
+    def takeFlight(self):
+        return self.outbound.pop(0)
 
 class Solution:
     def findItinerary(self, tickets: list ) -> list:
-        flights = []
-        for x in tickets:
-            flights.append(Flight(x))
-        print(flights)
-        minFlight = Flight(["NULL", "NULL"])
-        minFlightNum = 0
-        for i, flight in enumerate(flights):
-            if (flight.start == "JFK"):
-                if (flight.end < minFlight.end or minFlight.end == "NULL"):
-                    minFlight = flight 
-                    minFlightNum = i 
-        endCheck = False
-        while(not endCheck):
-            startFlight = flights[i]
+        airports = []
+        names = []
+        for flights in tickets:
+            if not flights[0] in names:
+                names.append(flights[0])
+                airports.append(Airport(flights[0]))
+            if not flights[1] in names:
+                names.append(flights[1])
+                airports.append(Airport(flights[1]))                
+        for flights in tickets:
+            finder = flights[0]
+            for i, x in enumerate(airports):
+                if finder == x.code:
+                    airports[i].addOutbound(flights[1])
+        for x in range(len(airports)):
+            airports[x].outbound.sort()
+        startIndex = 0
+        for i, x in enumerate(airports):
+            if x.code == "JFK":
+                startIndex = i
+                break
+        iten = ["JFK"]
+        while(len(airports) != 0):
+            if len(airports[startIndex].outbound) == 0:
+                if len()
+                break
+            currentFlight = airports[startIndex].takeFlight()
+            iten.append(currentFlight)
+            if len(airports[startIndex].outbound) == 0:
+                airports.pop(i)
+            for i, x in enumerate(airports):
+                if x.code == currentFlight:
+                    startIndex = i 
+                    break        
+
+        return iten
+
             
 
 
@@ -28,8 +54,9 @@ class Solution:
 def main():
     input1 = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
     input2 = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+    input3 = [["JFK","KUL"],["JFK","NRT"],["NRT","JFK"]]
     sol = Solution()
-    sol.findItinerary(input2)
+    sol.findItinerary(input1)
 
 if __name__ == "__main__":
     main()
